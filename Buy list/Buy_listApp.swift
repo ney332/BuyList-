96ad8cdd0d877224a3b00e02ab9c01fe4089ebed
lorran-mac <lorransantos059@gmail.com>
2025-12-23 21:50:20 -0300

    Initial Commit

---
+++ Buy list/Buy_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Buy_listApp.swift
+//  Buy list
+//
+//  Created by Lorran Silva on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Buy_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
